56266fc5fad49ae09b3d79c07cc3e9162114a53f
Carlos <carlos.sotod@sansano.usm.cl>
2022-06-04 18:03:31 -0400

    Uso de Sesiones implementado

--- Tarea_2_Grupo08/tarea2/server/create.php
+++ Tarea_2_Grupo08/tarea2/server/create.php
@@ -6,13 +6,16 @@
     $apellido = $_POST['apellido'];
     $email = $_POST['email'];
     $password = $_POST['password'];
+    $password_hashed = password_hash($password,PASSWORD_BCRYPT);
+    
 
-    $sql = "INSERT INTO personas(nombre, apellido, email, password, nombre_artistico, verificado, suscripcion_activa) VALUES ('$nombre', '$apellido', '$email', '$password', null, null, false);";
+    $sql = "INSERT INTO personas(nombre, apellido, email, password, nombre_artistico, verificado, suscripcion_activa) VALUES ('$nombre', '$apellido', '$email', '$password_hashed', null, null, false);";
 
     $result = pg_query($dbconn, $sql);
     
     if ($result) {
         echo("¡Guardado con éxito!");
+        header("Location: ../index.html");
     } else {
         echo("¡Error!");
     }
--- Tarea_2_Grupo08/tarea2/server/read.php
+++ Tarea_2_Grupo08/tarea2/server/read.php
@@ -4,12 +4,18 @@
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-    $sql = "SELECT nombre, apellido FROM personas WHERE email = '$email' AND password = '$password';";
+    $sql = "SELECT id,password FROM personas WHERE email = '$email';";
 
     $result = pg_query($dbconn, $sql);
+    $row = pg_fetch_row($result);
+    $password_hashed = $row[1];
 
-    if ($result) {
+    if (password_verify($password,$password_hashed)) {
         echo("!Obtenido con Éxito!");
+
+        session_start();
+        $_SESSION["id_artista"] = $row[0];
+        
         header("Location: ../html/frontpage.html");
     } else {
         echo("¡Error!");
