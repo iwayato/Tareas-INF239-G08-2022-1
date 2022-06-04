0cf6c241022d5d3694b97a084fd54a806e9cecc2
Carlos <carlos.sotod@sansano.usm.cl>
2022-06-04 18:54:40 -0400

    Añadido logica para evitar registro email repetido

--- Tarea_2_Grupo08/tarea2/server/create.php
+++ Tarea_2_Grupo08/tarea2/server/create.php
@@ -7,17 +7,24 @@
     $email = $_POST['email'];
     $password = $_POST['password'];
     $password_hashed = password_hash($password,PASSWORD_BCRYPT);
-    
 
-    $sql = "INSERT INTO personas(nombre, apellido, email, password, nombre_artistico, verificado, suscripcion_activa) VALUES ('$nombre', '$apellido', '$email', '$password_hashed', null, null, false);";
-
+    $sql = "SELECT * FROM personas WHERE email = '$email';";
     $result = pg_query($dbconn, $sql);
+    $row = pg_fetch_row($result);
+
+    if(isset($row[0])){
+        exit("Este email ya esta ocupado"); 
+    }
+    else{
+        $sql = "INSERT INTO personas(nombre, apellido, email, password, nombre_artistico, verificado, suscripcion_activa) VALUES ('$nombre', '$apellido', '$email', '$password_hashed', null, null, false);";
+        $result = pg_query($dbconn, $sql);
     
-    if ($result) {
-        echo("¡Guardado con éxito!");
-        header("Location: ../index.html");
-    } else {
-        echo("¡Error!");
+        if ($result) {
+            echo("¡Guardado con éxito!");
+            header("Location: ../index.html");
+        } else {
+            echo("¡Error!");
+        }
     }
        
 ?>
