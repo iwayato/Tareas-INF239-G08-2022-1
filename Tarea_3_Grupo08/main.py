1da749e46ec530512a88a5be2c1afc4bfbbac400
iwayato <iwayatomoaki@gmail.com>
2022-07-11 23:45:22 -0400

    Upload CRUD Personas

--- Tarea_3_Grupo08/main.py
+++ Tarea_3_Grupo08/main.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from config import config
-from models import db, Usuarios
+from models import db, Personas
 
 def create_app(enviroment):
 	app = Flask(__name__)
@@ -24,38 +24,36 @@ def get_personas():
     response = jsonify(personas)
     return response
 
-@app.route('/api/personas/<nombre>/<apellido>/<email>/<password>/<usuario_suscripcion_activa>/<artista_nombre_artistico>/<artista_verificado>/<tipo_de_persona>', methods = ['POST'])
+@app.route('/api/personas', methods = ['POST'])
 def put_personas():
     json = request.get_json()
     persona = Personas.create(
-		nombre = nombre, 
-		apellido = apellido, 
-		email = email, 
-		password = password, 
-		usuario_suscripcion_activa = usuario_suscripcion_activa, 
-		artista_nombre_artistico = artista_nombre_artistico,
-		artista_verificado = artista_verificado,
-		tipo_de_persona = tipo_de_persona
+		json['nombre'], 
+		json['apellido'], 
+		json['email'], 
+		json['password'], 
+		json['usuario_suscripcion_activa'], 
+		json['artista_nombre_artistico'],
+		json['artista_verificado'],
+		json['tipo_de_persona']
 	)
     response = jsonify(persona.json())
     return response
 
-@app.route('/api/personas/<nombre>/<apellido>/<email>/<password>/<usuario_suscripcion_activa>/<artista_nombre_artistico>/<artista_verificado>/<tipo_de_persona>', methods=['PUT'])
-def editar_persona(id):
-    json = request.get_json()
-    persona = Personas.query.filter_by(id=id).first()
-	# Aca VSCode tirra error
-    persona.nombre = nombre
-	persona.apellido = apellido
-	persona.email = email
-	persona.password = password
-	persona.usuario_suscripcion_activa = usuario_suscripcion_activa
-	persona.artista_nombre_artistico = artista_nombre_artistico
-	persona.artista_verificado = artista_verificado
-	persona.tipo_de_persona = tipo_de_persona
-    persona.update()
-	# hasta aca, averiguar porque (revisar documentacion)
-    return jsonify(persona.json())
+@app.route('/api/personas/<id>', methods=['PUT'])
+def editar_personas(id):
+	json = request.get_json()
+	persona = Personas.query.filter_by(id=id).first()
+	persona.nombre = json['nombre']
+	persona.apellido = json['apellido']
+	persona.email = json['email']
+	persona.password = json['password']
+	persona.usuario_suscripcion_activa = json['usuario_suscripcion_activa']
+	persona.artista_nombre_artistico = json['artista_nombre_artistico']
+	persona.artista_verificado = json['artista_verificado']
+	persona.tipo_de_persona = json['tipo_de_persona']
+	persona.update()
+	return jsonify(persona.json())
 
 @app.route('/api/personas/<id>', methods=['DELETE'])
 def delete_persona(id):
