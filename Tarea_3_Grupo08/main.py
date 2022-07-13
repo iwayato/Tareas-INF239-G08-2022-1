7e411c2a5a5ccdd93781238a9b2b7152504f9615
iwayato <iwayatomoaki@gmail.com>
2022-07-12 20:58:00 -0400

    Upload CRUD : Facturas, Canciones

--- Tarea_3_Grupo08/main.py
+++ Tarea_3_Grupo08/main.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from config import config
-from models import db, Personas
+from models import db, Personas, Facturas, Canciones, Reproducciones
 
 def create_app(enviroment):
 	app = Flask(__name__)
@@ -60,6 +60,114 @@ def delete_persona(id):
     persona = Personas.query.filter_by(id=id).first()
     persona.delete()
     return jsonify({'Mensaje':'Persona borrado'})
+
+#CRUD para las facturas
+@app.route('/api/facturas', methods=['GET'])
+def get_facturas():
+    facturas = [factura.json() for factura in Facturas.query.all()]
+    response = jsonify(facturas)
+    return response
+
+@app.route('/api/facturas', methods = ['POST'])
+def put_facturas():
+    json = request.get_json()
+    factura = Facturas.create(
+		json['monto_facturado'], 
+		json['fecha_facturacion'], 
+		json['fecha_vencimiento'], 
+		json['estado'], 
+		json['metodo_de_pago'], 
+		json['fecha_hora_pago'],
+		json['id_usuario']
+	)
+    response = jsonify(factura.json())
+    return response
+
+@app.route('/api/facturas/<id>', methods=['PUT'])
+def editar_facturas(id):
+	json = request.get_json()
+	factura = Facturas.query.filter_by(id=id).first()
+	factura.monto_facturado = json['monto_facturado']
+	factura.fecha_facturacion = json['fecha_facturacion']
+	factura.vencimiento = json['fecha_vencimiento']
+	factura.estado = json['estado']
+	factura.metodo_de_pago = json['metodo_de_pago']
+	factura.fecha_hora_pago = json['fecha_hora_pago']
+	factura.id_usuario = json['id_usuario']
+	factura.update()
+	return jsonify(factura.json())
+
+@app.route('/api/facturas/<id>', methods=['DELETE'])
+def delete_factura(id):
+    factura = Facturas.query.filter_by(id=id).first()
+    factura.delete()
+    return jsonify({'Mensaje':'Factura borrada'})
+
+#CRUD para las canciones
+@app.route('/api/canciones', methods=['GET'])
+def get_canciones():
+    canciones = [cancion.json() for cancion in Canciones.query.all()]
+    response = jsonify(canciones)
+    return response
+
+@app.route('/api/canciones', methods = ['POST'])
+def put_canciones():
+    json = request.get_json()
+    cancion = Canciones.create(
+		json['nombre'], 
+		json['letra'], 
+		json['fecha_composicion']
+	)
+    response = jsonify(cancion.json())
+    return response
+
+@app.route('/api/canciones/<id>', methods=['PUT'])
+def editar_canciones(id):
+	json = request.get_json()
+	cancion = Canciones.query.filter_by(id=id).first()
+	cancion.nombre = json['nombre']
+	cancion.letra = json['letra']
+	cancion.fecha_composicion = json['fecha_composicion']
+	cancion.update()
+	return jsonify(cancion.json())
+
+@app.route('/api/canciones/<id>', methods=['DELETE'])
+def delete_cancion(id):
+    cancion = Canciones.query.filter_by(id=id).first()
+    cancion.delete()
+    return jsonify({'Mensaje':'Canción borrada'})
+
+#CRUD para las reproducciones
+@app.route('/api/reproducciones', methods=['GET'])
+def get_reproducciones():
+	reproducciones = [reproduccion.json() for reproduccion in Reproducciones.query.all()]
+	response = jsonify(reproducciones)
+	return response
+
+@app.route('/api/reproducciones', methods = ['POST'])
+def put_reproducciones():
+	json = request.get_json()
+	reproduccion = Reproducciones.create(
+		json['id_usuario'], 
+		json['id_cancion']
+	)
+	response = jsonify(reproduccion)
+	return response
+
+@app.route('/api/reproducciones/<id>', methods=['PUT'])
+def editar_reproducciones(id):
+	json = request.get_json()
+	reproduccion = Reproducciones.query.filter_by(id=id).first()
+	reproduccion.id_usuario = json['id_usuario']
+	reproduccion.id_cancion = json['id_cancion']
+	reproduccion.update()
+	return jsonify(reproduccion.json())
+
+@app.route('/api/reproducciones/<id>', methods=['DELETE'])
+def delete_reproduccion(id):
+    reproduccion = Reproducciones.query.filter_by(id=id).first()
+    reproduccion.delete()
+    return jsonify({'Mensaje':'Reproducción borrada'})
 
 if __name__ == '__main__':
 	app.run(debug=True)
--- Tarea_3_Grupo08/models.py
+++ Tarea_3_Grupo08/models.py
@@ -2,7 +2,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-#Creamos tablas
+#Creación de las relaciones
+
 class Personas(db.Model):
     __tablename__ = 'personas'
     id = db.Column(db.Integer, primary_key = True)
@@ -15,9 +16,11 @@ class Personas(db.Model):
     artista_verificado = db.Column(db.Boolean, nullable = True)
     tipo_de_persona = db.Column(db.Boolean, nullable = True)
 
+    facturas = db.relationship('Facturas', cascade = "all, delete-orphan")
+    reproducciones = db.relationship('Reproducciones', cascade = "all, delete-orphan")
+
     @classmethod
     def create(cls, nombre, apellido, email, password, usuario_suscripcion_activa, artista_nombre_artistico, artista_verificado, tipo_de_persona):
-
         persona = Personas(
             nombre = nombre,
             apellido = apellido,
@@ -60,4 +63,144 @@ class Personas(db.Model):
             'artista_nombre_artistico': self.artista_nombre_artistico,
             'artista_verificado': self.artista_verificado,
             'tipo_de_persona': self.tipo_de_persona
+        }
+
+class Reproducciones(db.Model):
+    __tablename__ = 'reproducciones'
+    id = db.Column(db.Integer, primary_key = True)
+    id_usuario = db.Column(db.Integer, db.ForeignKey("personas.id"), primary_key = True)
+    id_cancion = db.Column(db.Integer, db.ForeignKey("canciones.id"), primary_key = True)
+
+    @classmethod
+    def create(cls, id_usuario, id_cancion):
+        reproduccion = Reproducciones(
+            id_usuario = id_usuario,
+            id_cancion = id_cancion
+        )
+        return reproduccion.save()
+
+    def save(self):
+        try:
+            db.session.add(self)
+            db.session.commit()
+            return self
+        except:
+            return False
+
+    def update(self):
+        self.save()
+
+    def delete(self):
+        try:
+            db.session.delete(self)
+            db.session.commit()
+            return self
+        except:
+            return False
+
+    def json(self):
+        return {
+            'id': self.id,
+            'id_usuario': self.id_usuario,
+            'id_cancion': self.id_cancion
+        }
+
+class Facturas(db.Model):
+    __tablename__ = 'facturas'
+    id = db.Column(db.Integer, primary_key = True)
+    monto_facturado = db.Column(db.Integer, nullable  = True)
+    fecha_facturacion = db.Column(db.Date, nullable = True)
+    fecha_vencimiento = db.Column(db.Date, nullable = True)
+    estado = db.Column(db.Boolean, nullable = True)
+    metodo_de_pago = db.Column(db.String(100), nullable = True)
+    fecha_hora_pago = db.Column(db.DateTime, nullable = True)
+    id_usuario = db.Column(db.Integer, db.ForeignKey("personas.id"))
+
+    @classmethod
+    def create(cls, monto_facturado, fecha_facturacion, fecha_vencimiento, estado, metodo_de_pago, fecha_hora_pago, id_usuario):
+        factura = Facturas(
+            monto_facturado = monto_facturado,
+            fecha_facturacion = fecha_facturacion,
+            fecha_vencimiento = fecha_vencimiento,
+            estado = estado,
+            metodo_de_pago = metodo_de_pago,
+            fecha_hora_pago = fecha_hora_pago,
+            id_usuario = id_usuario
+        )
+        return factura.save()
+
+    def save(self):
+        try:
+            db.session.add(self)
+            db.session.commit()
+            return self
+        except:
+            return False
+
+    def update(self):
+        self.save()
+
+    def delete(self):
+        try:
+            db.session.delete(self)
+            db.session.commit()
+            return self
+        except:
+            return False
+
+    def json(self):
+        return {
+            'id': self.id,
+            'monto_facturado': self.monto_facturado,
+            'fecha_facturacion': self.fecha_facturacion,
+            'fecha_vencimiento': self.fecha_vencimiento,
+            'estado': self.estado,
+            'metodo_de_pago': self.metodo_de_pago,
+            'fecha_hora_pago': self.fecha_hora_pago,
+            'id_usuario': self.id_usuario
+        }
+
+class Canciones(db.Model):
+    __tablename__ = 'canciones'
+    id = db.Column(db.Integer, primary_key = True)
+    nombre = db.Column(db.String(100), nullable = False)
+    letra = db.Column(db.String(1000), nullable = True)
+    fecha_composicion = db.Column(db.Date, nullable = True)
+
+    reproducciones = db.relationship('Reproducciones', cascade = "all, delete-orphan")
+
+    @classmethod
+    def create(cls, nombre, letra, fecha_composicion):
+        cancion = Canciones(
+            nombre = nombre,
+            letra = letra,
+            fecha_composicion = fecha_composicion
+        )
+        return cancion.save()
+
+    def save(self):
+        try:
+            db.session.add(self)
+            db.session.commit()
+            return self
+        except:
+            return False
+
+    def update(self):
+        self.save()
+
+    def delete(self):
+        try:
+            db.session.delete(self)
+            db.session.commit()
+            return self
+        except:
+            return False
+
+    def json(self):
+        return {
+            'id': self.id,
+            'nombre': self.nombre,
+            'letra': self.letra,
+            'fecha_composicion': self.fecha_composicion
         }
